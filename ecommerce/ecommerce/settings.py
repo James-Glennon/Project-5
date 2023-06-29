6719162684a64778566e8fd265688a38e02aadc6
James-Glennon <>
2023-06-29 01:58:37 +0000

    heroku deployment troubleshooting

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-n2j+sk1o13n7r9o8)6w^ri44cb+i(q^eeym_!5o29m%u0t+q!1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-james-glennon-project-5-oiye3lxzm4.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-james-glennon-project-5-oiye3lxzm4.us2.codeanyapp.com',
+                 'ecommerce.heroku.com',]
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ecommerce',
     'home',
 ]
 
